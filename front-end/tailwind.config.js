c6bdec11c298af6257e85b4ee6364164b0ba0651
boytur <104257779+boytur@users.noreply.github.com>
2024-12-07 12:25:03 +0700

    🔧 chore[tailwind]: clean up tailwind.config.js formatting

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/preline/dist/*.js",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/preline/dist/*.js'
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [
-    require('preline/plugin'),
-  ],
+    extend: {}
+  }
 }
